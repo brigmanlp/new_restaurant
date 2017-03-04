9482dd6feb58eb2f1dec11357bbd36a1e2461e6a
Jessica Pankus <jessica@wifi_stu-10-38-80-213.uncc.edu>
2017-03-04 11:13:58 -0500

    updated api path in server.js for url links

--- server.js
+++ server.js
@@ -40,10 +40,14 @@ var tables = [{
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reservation", function(req, res) {
     res.sendFile(path.join(__dirname, "reservation.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
